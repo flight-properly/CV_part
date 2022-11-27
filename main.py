f20e7257ee4c427f38ee237b588625e9b454247a
Huere <56443524+elppa1200@users.noreply.github.com>
2022-11-27 23:57:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -82,39 +82,35 @@ with mp_hands.Hands(model_complexity=0, min_detection_confidence=0.7, min_tracki
                 left_9_y = left_hand.landmark[9].y * image_height
 
                 # save initial value
-                if swt == 0:
-                    init_width = (((left_5_x - left_17_x) ** 2 + (left_5_y - left_17_y) ** 2) ** 0.5 + ((right_5_x - right_17_x) ** 2 + (right_5_y - right_17_y) ** 2) ** 0.5) / 2
-                    init_height = (right_5_y + left_5_y) / 2
-                    init_throttle = (((left_4_x - left_6_x) ** 2 + (left_4_y - left_6_y) ** 2) ** 0.5 + ((right_4_x - right_6_x) ** 2 + (right_4_y - right_6_y) ** 2) ** 0.5) / 2
-                    swt = 1
+                if (swt==0): 
+                  init_width=(((left_5_x-left_17_x)**2+(left_5_y-left_17_y)**2)**0.5+((right_5_x-right_17_x)**2+(right_5_y-right_17_y)**2)**0.5)/2
+                  init_height=(right_5_y +left_5_y)/2
+                  init_throttle=(((left_4_x-left_6_x)**2+(left_4_y-left_6_y)**2)**0.5+((right_4_x-right_6_x)**2+(right_4_y-right_6_y)**2)**0.5)/2
+                  swt=1
 
-                # pitch
-                depth_avg = ((((left_5_x - left_17_x) ** 2 + (left_5_y - left_17_y) ** 2) ** 0.5 + ((right_5_x - right_17_x) ** 2 + (right_5_y - right_17_y) ** 2) ** 0.5) / 2) / init_width
-                if depth_avg > 1:
-                    depth_avg = 1.0
-                elif depth_avg < 0:
-                    depth_avg = 0.0
+                #pitch 
+                depth_avg = ((((left_5_x-left_17_x)**2+(left_5_y-left_17_y)**2)**0.5+((right_5_x-right_17_x)**2+(right_5_y-right_17_y)**2)**0.5)/2)/init_width-0.4
+                if (depth_avg>1) : depth_avg=1.0
+                elif (depth_avg<0) : depth_avg=0.0
 
-                # roll&yaw
-                rollyaw_left = init_height - left_5_y
-                rollyaw_right = init_height - right_5_y
+                #roll&yaw
+                rollyaw_left=init_height-left_5_y
+                rollyaw_right=init_height-right_5_y
 
-                if rollyaw_left > 120 * diff or rollyaw_right < -120 * diff:  # right+
-                    real_height = (rollyaw_left + (rollyaw_right * -1)) / init_height
+                if (rollyaw_left>60.0 or rollyaw_right<-60.0):#right+
+                  real_height=(rollyaw_left+(rollyaw_right*-1))/init_height
 
-                elif rollyaw_left < -120 * diff or rollyaw_right > 120 * diff:  # left-
-                    real_height = (rollyaw_right + (rollyaw_left * -1)) / init_height
-                    real_height *= -1
+                elif (rollyaw_left<-60.0 or rollyaw_right>60.0):#left-
+                  real_height=(rollyaw_right+(rollyaw_left*-1))/init_height
+                  real_height*=-1
 
-                if real_height > 1:
-                    real_height = 1.0
-                elif real_height < -1:
-                    real_height = -1.0
+                if (real_height>1) : real_height=1.0
+                elif (real_height<-1) : real_height=-1.0
 
-                # throttle
-                throttle_left = ((left_4_x - left_6_x) ** 2 + (left_4_y - left_6_y) ** 2) ** 0.5
-                throttle_right = ((right_4_x - right_6_x) ** 2 + (right_4_y - right_6_y) ** 2) ** 0.5
-                if depth_avg < 0.5 and depth_avg > -0.5 and (init_throttle / throttle_right + init_throttle / throttle_right) / 2 > 1.5:
+                #throttle
+                throttle_left=((left_4_x-left_6_x)**2+(left_4_y-left_6_y)**2)**0.5
+                throttle_right=((right_4_x-right_6_x)**2+(right_4_y-right_6_y)**2)**0.5
+                if (depth_avg<0.5 and depth_avg>-0.5 and (init_throttle/throttle_right+init_throttle/throttle_right)/2 >1.5):
                     throttle = 1
                 else:
                     throttle = 0
