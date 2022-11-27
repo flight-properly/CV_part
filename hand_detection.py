5d6aeda2e343db1b794d5ab9788c02e1cb2774f8
Huere <56443524+elppa1200@users.noreply.github.com>
2022-11-27 22:16:21 +0900

    value logic adjusting

--- hand_detection.py
+++ hand_detection.py
@@ -6,16 +6,9 @@ import socket
 
 # server IP, PORT
 HOST = '127.0.0.1'
-PORT = 12325
+PORT = 1234
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
-print("Waiting for client to connect...")
-server_socket.listen()
-
-client_socket, addr = server_socket.accept()
-
-print("Connected from", addr)
+server_socket.connect((HOST, PORT))
 
 #define
 mp_drawing = mp.solutions.drawing_utils
@@ -35,7 +28,6 @@ with mp_hands.Hands(
   model_complexity=0,
   min_detection_confidence=0.7,
   min_tracking_confidence=0.7) as hands:
-
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -45,7 +37,7 @@ with mp_hands.Hands(
     image.flags.writeable = True
     
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (640, 480))
+    image = cv2.resize(image, (1920,1080))
     results = hands.process(image)
 
     image_height, image_width, _ = image.shape
@@ -67,17 +59,25 @@ with mp_hands.Hands(
 
         right_17_y = right_hand.landmark[17].y * image_height
         left_17_y = left_hand.landmark[17].y * image_height
+        
+        right_6_x = right_hand.landmark[6].x * image_width
+        left_6_x = left_hand.landmark[6].x * image_width
+        right_6_y = right_hand.landmark[6].y * image_height
+        left_6_y = left_hand.landmark[6].y * image_height
 
-        right_4_y = right_hand.landmark[4].y * image_width
-        left_4_y = left_hand.landmark[4].y * image_width
+        right_4_x = right_hand.landmark[4].x * image_width
+        left_4_x = left_hand.landmark[4].x * image_width
+        right_4_y = right_hand.landmark[4].y * image_height
+        left_4_y = left_hand.landmark[4].y * image_height
+        
+        right_9_y = right_hand.landmark[9].y * image_height
+        left_9_y = left_hand.landmark[9].y * image_height
 
-        right_9_y = right_hand.landmark[9].y * image_width
-        left_9_y = left_hand.landmark[9].y * image_width
-        #save initial width 
+        #save initial value
         if (swt==0): 
           init_width=((right_17_y - right_5_y) + (left_17_y - left_5_y))/2
-          init_height_right=right_5_y
-          init_height_left=left_5_y
+          init_height=(right_5_y +left_5_y)/2
+          init_throttle=(((left_4_x-left_6_x)**2+(left_4_y-left_6_y)**2)**0.5+((right_4_x-right_6_x)**2+(right_4_y-right_6_y)**2)**0.5)/2
           swt=1
 
         #pitch 
@@ -86,24 +86,41 @@ with mp_hands.Hands(
         elif (depth_avg<-1) : depth_avg=-1.0
       
         #roll&yaw
-        if (left_5_y-right_5_y > 20.0 or left_5_y-right_5_y < -20.0):
-          real_height = (left_5_y-right_5_y / 400 - 200) / -200
-        else: real_height=0.0
-        if (real_height<1 and real_height>0) : real_height*=2.5
+        rollyaw_left=init_height-left_5_y
+        rollyaw_right=init_height-right_5_y
+
+        if (rollyaw_left>120.0 or rollyaw_right<-120.0):#right+
+          real_height=(rollyaw_left+(rollyaw_right*-1))/init_height
+
+        elif (rollyaw_left<-120.0 or rollyaw_right>120.0):#left-
+          real_height=(rollyaw_right+(rollyaw_left*-1))/init_height
+          real_height*=-1
+
         if (real_height>1) : real_height=1.0
         elif (real_height<-1) : real_height=-1.0
         
         #throttle
-        if (depth_avg<0.5 and depth_avg>-0.5 and right_4_y > right_9_y-120 and left_4_y > left_9_y-120):
+        throttle_left=((left_4_x-left_6_x)**2+(left_4_y-left_6_y)**2)**0.5
+        throttle_right=((right_4_x-right_6_x)**2+(right_4_y-right_6_y)**2)**0.5
+        if (depth_avg<0.5 and depth_avg>-0.5 and (init_throttle/throttle_right+init_throttle/throttle_right)/2 >1.5):
             throttle = 1
         else:
             throttle = 0
 
-        body = json.dumps({ "pitch": -depth_avg, "roll": real_height, "yaw": -real_height, "throttle": throttle })
-        client_socket.sendall(body.encode())
-        print(json.loads(body))
+
+        #output
+        header = []
+        header.append(0x20)
+
+        body = json.dumps({"pitch":depth_avg,"rollyaw":real_height,"throttle":throttle})
+        rst=bytearray(header)
+        rst+=bytearray((3).to_bytes(2, byteorder='big'))
+        rst+=bytes(body,'utf-8')
+
+        server_socket.sendall(rst)
+        data = server_socket.recv(1024)
+        # print(depth_avg, real_height,throttle)
 
     except:
       print()
-client_socket.close()
 server_socket.close()
